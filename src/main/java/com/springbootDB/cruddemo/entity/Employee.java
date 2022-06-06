deb09112998d4d0b88349eb30d84a6e015349171
udhay <contactudhayaanandan@gamil.com>
2022-06-06 22:19:05 +0530

    Intial Commit

---
+++ src/main/java/com/springbootDB/cruddemo/entity/Employee.java
@@ -0,0 +1,94 @@
+package com.springbootDB.cruddemo.entity;
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employee")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Employee {
+    //Define Fields
+
+    //primary Key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+//    @Transient
+//    private String Status="Success";
+
+    //Define Constructors
+    //Creating No argument Constructors its needed by Hibernate
+
+  /*  public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }*/
+
+
+    //Define getter/Setter
+/*
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //Define toString
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }*/
+}
