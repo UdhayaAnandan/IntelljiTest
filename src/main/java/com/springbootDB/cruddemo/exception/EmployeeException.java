078647540866c84671853aa8ad1a0388828a9c77
udhay <contactudhayaanandan@gamil.com>
2022-06-06 22:25:10 +0530

    Intial Commit

---
+++ src/main/java/com/springbootDB/cruddemo/exception/EmployeeException.java
@@ -0,0 +1,16 @@
+package com.springbootDB.cruddemo.exception;
+
+
+public class EmployeeException  extends  RuntimeException{
+    public EmployeeException(String message) {
+        super(message);
+    }
+
+    public EmployeeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmployeeException(Throwable cause) {
+        super(cause);
+    }
+}
